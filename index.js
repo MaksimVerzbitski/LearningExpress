aa229152bcd14e888f8bd7656c54dd9c5601be70
MaksimVerzbitski <maksim.verzbitski@gmail.com>
2023-01-17 11:57:53 +0200

    Prisma lesson

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-const express = require('express')
+import express, { response } from 'express';
+import nunjucks from 'nunjucks';
+import path from 'path';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+/* const express = require('express')
 const path = require('path');
-const nunjucks = require('nunjucks');
+const nunjucks = require('nunjucks'); */
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
 
 app.set('views', './views');
 
-app.get('/', (request, response) => {
-    response.render('index.njk');
-});
 
-app.get('/page1', (request, response) => {
-    response.render('index.njk');
-  });
+app.get('/', async (request, response) => {
+    const users = await prisma.user.findMany();
+    await prisma.$disconnect();
+    console.log(users);
+    response.render('index.njk', { users });
+});
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+    autoescape:true,
+    express:app
+})
+
+app.get('/register', (request, response) => {
+    console.log(request.quary)
+    response.render('register.njk');
 });
+
+app.post('/register', async (request, response) => {
+  console.log(request.body);
+  const user = await prisma.user.create({
+    data: {
+      name: request.body.name,
+      email: request.body.email,
+    },
+  });
+  await prisma.$disconnect();
+  console.log(user)
+  response.redirect("/");
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
